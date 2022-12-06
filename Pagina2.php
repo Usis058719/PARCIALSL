d0ea273640f35d1d12812d19eb1e140f05ac22ba
Nelson Josuè Cerna Serrano <60446304+Usis058719@users.noreply.github.com>
2022-12-05 23:16:30 -0600

    Create Pagina2.php

---
+++ Pagina2.php
@@ -0,0 +1,11 @@
+
+<?php
+$nombre = $_POST["Nelson"];
+$apellido = $_POST["Serrano"];
+$email = $_POST["serranonelson24@gmail.com"]; 
+echo "Hola " . $nombre . " " . 
+$apellido . ", <br /> 
+Te has registrado con el siguiente 
+correo electrónico: " . $email . 
+".";
+?>
