36416fe0b7e36f2a6cbd779cfadb36ddf9a2d533
Nelson Josuè Cerna Serrano <60446304+Usis058719@users.noreply.github.com>
2022-12-05 23:13:03 -0600

    Create Pagina1.php

---
+++ Pagina1.php
@@ -0,0 +1,13 @@
+<form method="post" 
+action="pagina2.php" >
+Por favor, envía la newsletter a: <br 
+/>
+Tu nombre: <input type="text" 
+name="Nelson" /><br />
+Tu apellido: <input type="text" 
+name="Serrano" /><br />
+Tu correo electrónico: <input 
+type="text" name="serranonelson24@gmail.com" /><br />
+<input type="submit" value="Enviar 
+formulario" />
+</form>
